2eb29e5eb5810247c33d283043b018ec1d716610
Shahroz135 <116105635+Shahroz135@users.noreply.github.com>
2023-01-18 17:03:53 +0500

    Add files via upload

---
+++ all_albhabet.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+#include"conio.h"
+void main()
+{
+    char c;
+    for(c='A';c<='Z';c++)
+    {
+        printf("\n%c",c);
+    }
+    getch();
+}
+    
---
+++ rev_albhabet.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+#include"conio.h"
+void main()
+{
+    char c;
+    for(c='Z';c>='A';c--)
+    {
+        printf("\n%c",c);
+    }
+    getch();
+}
+    
